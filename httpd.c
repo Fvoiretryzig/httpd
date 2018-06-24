af83c626e281daf782accc8617441329d111a66e
tracer <tracer@tracer.org>
2018-06-24 11:12:25 +0800

    >  compile forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -216,7 +216,7 @@ int main(int argc, char *argv[])
 	}
 	memset(server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);	
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
     	printf("\033[41;37mbind failed!!!!\033[0m\n");
