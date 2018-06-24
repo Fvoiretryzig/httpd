5f6414cd838eace9544795b84341db12d9db5db1
tracer <tracer@tracer.org>
2018-06-24 15:38:57 +0800

    >  compile forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -132,6 +132,7 @@ void make_response(int client, char* file_path)
 }
 void *request_parse(void* arg)
 {
+	printf("in request_parse\n");
 	int client = (int)(int *)arg;
 	char buf[1024]; char file_path[256]; char url[128]; char method[256];
 	int line_len = 0;
