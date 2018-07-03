87a8bb0e1f2d7534de67980bf6356f7a544b72d0
tracer <tracer@tracer.org>
2018-07-03 14:02:10 +0800

    >  compile forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -289,8 +289,7 @@ int main(int argc, char *argv[])
 	    printf("argv[%d] = %s\n", i, argv[i]);
 	}	
 	assert(!argv[argc]); // specification	
-	printf("argc:%d\n", argc);
-	if(argc < 1){
+	if(argc < 2){
 		printf("\033[41;37mINVALID cmd!!!! Enter ./httpd -h to look for help!\033[0m\n");
 		exit(1);
 	}
